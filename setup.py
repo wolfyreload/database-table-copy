810f7e30df096310860e6e9f397dfa07fb46e7df
wolfyreload <open.chords.app@gmail.com>
2023-10-28 11:17:05 +0200

    Create Version 1.0.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ include_files = [("config.example.json", "config.example.json"),
 
 setup(
     name='DatabaseTableCopy',
-    version='1.0.1',
+    version='1.0.2',
     description='A bcp wrapper for copy table data between two databases',
     executables=[executable],
     options={'build_exe':
